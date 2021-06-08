06857d4fe2a2c9e5491673b3cf0e74f8a3e1557a
Ashraf Latif <hello@ceghap.com>
2021-06-08 15:42:39 +0800

    add abstract factory pattern

---
+++ abstract-factory.js
@@ -0,0 +1,46 @@
+class Car {
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+class MercedesFactory {
+  create(name) {
+    switch (name) {
+      case "C300":
+        return new Car(name, 2000, "This is C300");
+      case "Maybach":
+        return new Car(name, 5000, "This is Maybach");
+      default:
+        return null;
+    }
+  }
+}
+
+class BMWFactory {
+  create(name) {
+    switch (name) {
+      case "X5":
+        return new Car(name, 1000, "This is X5");
+      case "X6":
+        return new Car(name, 2000, "This is X6");
+      default:
+        return null;
+    }
+  }
+}
+
+class CarAbstractFactory {
+  static getFactory(type) {
+    switch (type) {
+      case "Mercedes":
+        return new MercedesFactory();
+      case "BMW":
+        return new BMWFactory();
+      default:
+        return null;
+    }
+  }
+}
